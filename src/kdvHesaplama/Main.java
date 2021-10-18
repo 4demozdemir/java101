76560f7bfae2e75dc1c115026bfe12f85f163e1b
adem özdemir <ozadem.98@gmail.com>
2021-10-18 22:33:17 +0300

    kdvhesaplama package oluşturuldu

--- src/kdvHesaplama/Main.java
+++ src/kdvHesaplama/Main.java
@@ -1,4 +1,26 @@
 package kdvHesaplama;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+       Scanner input = new Scanner(System.in);
+       double priceFirst,kdv,priceLast;
+       System.out.println("Fiyat girin lütfen");
+        priceFirst= input.nextInt();
+       if(priceFirst<1000){
+           kdv= priceFirst * 0.18;
+
+       }else
+       {
+           kdv= priceFirst *0.8;
+
+       }
+       priceLast= priceFirst+kdv;
+       System.out.println("Kdv'siz Fiyat : " +priceFirst);
+            System.out.println("kdv : "+ kdv);
+        System.out.println("Kdv'siz Fiyat : " +priceLast);
+
+    }
+
 }
