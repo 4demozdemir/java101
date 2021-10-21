3835e08e48093893813f230c68606c76545bc7fd
adem özdemir <ozadem.98@gmail.com>
2021-10-22 01:54:13 +0300

    manav kasa program  kodu yazıldı...

--- src/manavKasaProgramı/Main.java
+++ src/manavKasaProgramı/Main.java
@@ -7,16 +7,16 @@ public class Main {
         double elma,armut,domates,muz,patlican,toplam;
         double elmaF=3.67,armutF=2.14 ,domatesF=1.11,muzF=0.95,patlicamF=5.00;
         Scanner input = new Scanner(System.in);
-        System.out.println("Ürün bilgisi : ");
-        System.out.println("Elma");
+
+        System.out.println("Elma kaç kilo ?");
         elma=input.nextDouble();
-        System.out.println("Armut ");
+        System.out.println("Armut kaç kilo ? ");
         armut=input.nextDouble();
-        System.out.println("Muz ");
+        System.out.println("Muz kaç kilo ?");
         muz=input.nextDouble();
-        System.out.println("Domates");
+        System.out.println("Domates kaç kilo ?" );
         domates=input.nextDouble();
-        System.out.println("Patlican");
+        System.out.println("Patlican kaç kilo ?");
         patlican=input.nextDouble();
 toplam=(elma*elmaF)+ (armut * armutF)+ (domates * domatesF) + (muz*muzF)+ (patlicamF* patlican);
         System.out.println("Toplam : " + toplam);
