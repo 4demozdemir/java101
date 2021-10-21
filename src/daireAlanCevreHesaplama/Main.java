d208fd70d54dd5519a9775730bcd0d02afa3d1a5
adem özdemir <ozadem.98@gmail.com>
2021-10-21 22:21:21 +0300

    daire çevre alan  hesaplama kodu yazıldı...

---
+++ src/daireAlanCevreHesaplama/Main.java
@@ -0,0 +1,15 @@
+package daireAlanCevreHesaplama;
+import java.util.Scanner;
+import static java.lang.System.in;
+public class Main {
+    public  static void main(String[]args){
+       Scanner input = new Scanner(in);
+       System.out.println("Yarıçap uzunluğunu giriniz ? ");
+       double radius=input.nextDouble(),alan,cevre;
+            alan = Math.PI * radius *radius;
+            cevre= Math.PI *radius * 2 ;
+    System.out.println("Alan : " + alan );
+    System.out.println("Cevre : s"+ cevre);
+
+    }
+}
